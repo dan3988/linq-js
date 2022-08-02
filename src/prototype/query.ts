60390429ed38eccf6e119ed240c4a55bc448209f
daniel.pickett@workmobileforms.com <daniel.pickett@workmobileforms.com>
2022-08-02 12:55:53 +0100

    Fixed build errors

--- src/prototype/query.ts
+++ src/prototype/query.ts
@@ -1,49 +1,49 @@
-import { AsyncLinqExtend, LinqExtend } from '../impl/extend.js';
+import { AsyncLinqExtend, LinqExtend, OperationType } from '../impl/extend.js';
 import { Linq, AsyncLinq, LinqInternal } from '../linq-base.js';
 import { getter, isInstance, isType, Predictate, SelectType } from '../util.js';
 
 LinqInternal.prototype.where = function(filter: Predictate) {
-	return new LinqExtend(this, 'filter', filter);
+	return new LinqExtend(this, OperationType.Filter, filter);
 }
 
 AsyncLinq.prototype.where = function(filter: Predictate) {
-	return new AsyncLinqExtend(this, 'filter', filter);
+	return new AsyncLinqExtend(this, OperationType.Filter, filter);
 }
 
 LinqInternal.prototype.select = function(query: SelectType) {
 	if (typeof query != 'function')
 		query = getter.bind(undefined, query);
 
-	return new LinqExtend(this, 'select', query);
+	return new LinqExtend(this, OperationType.Select, query);
 }
 
 AsyncLinq.prototype.select = function(query: SelectType) {
 	if (typeof query != 'function')
 		query = getter.bind(undefined, query);
 
-	return new AsyncLinqExtend(this, 'select', query);
+	return new AsyncLinqExtend(this, OperationType.Select, query);
 }
 
 LinqInternal.prototype.selectMany = function(query: SelectType) {
 	if (typeof query != 'function')
 		query = getter.bind(undefined, query);
 
-	return new LinqExtend(this, 'selectMany', query);
+	return new LinqExtend(this, OperationType.SelectMany, query);
 }
 
 AsyncLinq.prototype.selectMany = function(query: SelectType) {
 	if (typeof query != 'function')
 		query = getter.bind(undefined, query);
 
-	return new AsyncLinqExtend(this, 'selectMany', query);
+	return new AsyncLinqExtend(this, OperationType.SelectMany, query);
 }
 
 LinqInternal.prototype.ofType = function(type: string | Function): Linq<any> {
 	let func = typeof type === 'string' ? isType.bind(undefined, type) : isInstance.bind(undefined, type);
-	return new LinqExtend(this, 'filter', func);
+	return new LinqExtend(this, OperationType.Filter, func);
 }
 
 AsyncLinq.prototype.ofType = function(type: string | Function): AsyncLinq<any> {
 	let func = typeof type === 'string' ? isType.bind(undefined, type) : isInstance.bind(undefined, type);
-	return new AsyncLinqExtend(this, 'filter', func);
+	return new AsyncLinqExtend(this, OperationType.Filter, func);
 }
